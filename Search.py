81521874b3cb3d85fc5c9f0e52027cc490638a90
sugarraysam <sam_dowdy@hotmail.com>
2017-10-07 13:14:52 -0400

    Search.py

--- Search.py
+++ Search.py
@@ -1,17 +1,63 @@
 import networkx as nx
+import structs
 
 class Search():
     def __init__(self, graph, player):
         self.graph = graph
         self.player = player
-        self.interest_points = dict()
+        # update home ressources quand marche sur maison
+        self.home_ressources = 0
+        self.total_ressources = 0
+
+        self.home_interests_points = {}
+        self.interest_points = {}
+        # list of objectives [ (condition, action) ]
+        self.upgrade_queue = [
+                ("self.total_ressources == 15000", "self.go_upgrade(capacity)"), # update carrying capacity
+                #("self.total_ressources == 15000", self.go_upgrade()
+                    
+
+                ]
 
     # Return all nodes from start to end
     def astar(self, start, end):
         return nx.astar_path(self.graph, start, end)
     
-    # 
-    def breadth_search(self):
-        return nx.algorithms.coloring.strategy_connected_sequential(self.graph, "", traversal='bfs')
+    # return shortest path to home
+    def go_home(self):
+        home = self.player.HouseLocation.to_tuple()
+        pos = self.player.Position.to_tuple()
+        return self.astar(self.graph, pos, home)
+
+    # action to do every beginning
+    def indispendable_checks(self):
+        # ajuster ressources si maison
+        if self.player.Position == self.player.HouseLocation:
+            self.home_ressources += self.player.CarriedRessources
+            self.total_ressources = self.home_ressources
+
+        # find if capacity is full
+        if self.player.CarriedRessources == self.player.CarryingCapacity:
+            self.go_home()
+
+        # find if ready for upgrade (execute first objective priority list)
+        if eval(self.upgrade_queue[0][0]):
+            action = self.upgrade_queue[0][1]
+            self.upgrade_queue.pop(0)
+            eval(action)
+
+
+    
+
+    # find closets mining ressources
+    def find_mine(self):
+
+    # go to upgrade
+    def go_upgrade(self):
+        return 
+
+
+
+
 
 
