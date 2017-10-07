6a4291a14f62c79d1aa4cb56b7f9effc0dc09e3e
sugarraysam <sam_dowdy@hotmail.com>
2017-10-07 12:21:31 -0400

    Search.py

---
+++ Search.py
@@ -0,0 +1,17 @@
+import networkx as nx
+
+class Search():
+    def __init__(self, graph, player):
+        self.graph = graph
+        self.player = player
+        self.interest_points = dict()
+
+    # Return all nodes from start to end
+    def astar(self, start, end):
+        return nx.astar_path(self.graph, start, end)
+    
+    # 
+    def breadth_search(self):
+        return nx.algorithms.coloring.strategy_connected_sequential()
+
+
